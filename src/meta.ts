bdb76fb8b61362ddf3eb75ef5a5019d60cd4846f
potatolover68 <111818540+potatolover68@users.noreply.github.com>
2026-07-16 21:00:57 -0700

    fix: oops

--- src/meta.ts
+++ src/meta.ts
@@ -4,7 +4,7 @@ export const APP_NAME = "pillbug";
 export const APP_VERSION = pkg.version;
 export const GITHUB_REPO = "https://github.com/potatolover68/pillbug";
 export const GITHUB_ISSUES_NEW = `${GITHUB_REPO}/issues/new`;
-export const GITHUB_WIKI = `${GITHUB_REPO}/wiki/Getting_started`;
+export const GITHUB_WIKI = `${GITHUB_REPO}/wiki/Getting-started`;
 
 /** Fixed UA sent to MediaWiki; not user-configurable. */
 export const DEFAULT_USER_AGENT = `${APP_NAME}/${APP_VERSION} (${GITHUB_REPO})`;
